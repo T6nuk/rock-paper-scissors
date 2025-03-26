79dac7fc609a977c45ef083482c46a7e50e40783
T6nu <74606960+T6nuk@users.noreply.github.com>
2025-03-26 13:50:07 +0000

    Modify playRound function to include 5 rounds + count score

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
 
 function getComputerChoice() {
     const rand = Math.floor(Math.random() * 3) + 1
-    console.log(rand);
         if (rand == 1) {
             return 'Rock'
             // console.log('rock');
@@ -43,7 +42,11 @@ function getHumanChoice() {
 
 getHumanChoice();
 
-function playRound(human) {
+
+
+
+
+function playRound() {
     document.getElementById('play-button').addEventListener(
         'click',
         function () {
@@ -51,31 +54,63 @@ function playRound(human) {
             const computerSelection = getComputerChoice();
             document.getElementById('computers-pick').innerHTML
             = 'Computers choice: '+computerSelection;
-
-            //clear winner field
             document.getElementById('declare-winner').innerHTML
             = ' '
             
-            //user input value
-            console.log(humanSelection);
-            
+
+            let whoWon = '';
+            let humanScore = 0;
+            let computerScore = 0;
+
             if (humanSelection == 'Rock' && computerSelection == 'Scissors') {
                 document.getElementById('declare-winner').innerHTML
                 += ' Player won'
-            } else if (humanSelection == 'Scissors' && computerSelection == 'Paper') {
-                document.getElementById('declare-winner').innerHTML
-            += ' Player won'
+                whoWon = 'player'
                 
-            } else if (humanSelection == 'Paper' && computerSelection == 'Rock') {
+                
+            }   else if (humanSelection == computerSelection) {
                 document.getElementById('declare-winner').innerHTML
-            += ' Player won'
-            } else if (humanSelection == computerSelection) {
+                += 'It is a draw!'
+            }   else if (humanSelection == 'Scissors' && computerSelection == 'Paper') {
                 document.getElementById('declare-winner').innerHTML
-            += 'It is a draw!'
-            } else {
+                += ' Player won';
+                whoWon = 'player'
+                
+            }   else if (humanSelection == 'Paper' && computerSelection == 'Rock') {
                 document.getElementById('declare-winner').innerHTML
-            += ' Computer won'
-            }    
+                += ' Player won'
+                whoWon = 'player'
+            }   else {
+                document.getElementById('declare-winner').innerHTML
+                += ' Computer won'
+                whoWon = 'computer'
+            }   
+
+                if (whoWon == 'player') {
+                    humanScore++;
+                    let numberBox = document.getElementById('human-score');
+                    let currentNumber = parseInt(numberBox.textContent, 0)
+                    
+                    numberBox.textContent = currentNumber + humanScore; 
+                    console.log('this is human score: ' +currentNumber);
+
+                    if (currentNumber + 1 == 5) {
+                        alert('Game over, player won!')
+                        window.location.reload();
+                    }
+
+                } else if (whoWon == 'computer') {
+                    computerScore++;
+                    let numBoxComputer = document.getElementById('computer-score');
+                    let currentComputerNumber = parseInt(numBoxComputer.textContent, 0)
+
+                    numBoxComputer.textContent = currentComputerNumber + computerScore;
+                    
+                    if (currentComputerNumber + 1 == 5) {
+                        alert('Game over, player won!')
+                        window.location.reload();
+                    }
+                }
         }
     )
 }
