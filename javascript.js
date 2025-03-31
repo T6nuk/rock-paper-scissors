1fb78f7cfa0efd0bb1206cb990783b6ac00afc76
T6nu <74606960+T6nuk@users.noreply.github.com>
2025-03-31 17:46:17 +0000

    Change getElement to queryselector and innerHTML to textConcent

--- javascript.js
+++ javascript.js
@@ -1,107 +1,84 @@
-
 function getComputerChoice() {
-    const rand = Math.floor(Math.random() * 3) + 1
-        if (rand == 1) {
-            return 'Rock'
-        } else if (rand == 2) {
-            return 'Paper'
-        } else {
-            return 'Scissors'
-        }
+  const rand = Math.floor(Math.random() * 3) + 1;
+  if (rand == 1) {
+    return "Rock";
+  } else if (rand == 2) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
 }
 
 getComputerChoice();
 
-
-let userChoice = '';
+let userChoice = "";
 function getHumanChoice() {
-    
-    let getOption = document.getElementById('user-choice').
-    addEventListener('click', 
-        function () {
-            document.getElementById('users-pick').
-            innerHTML = '';
-            userChoice = document.getElementById('options').value;
-            document.getElementById('users-pick').
-            innerHTML += 'Your choice: ' +userChoice;
-            return userChoice;
-        }
-    )
+  let getOption = document
+    .querySelector("#user-choice")
+    .addEventListener("click", function () {
+      document.querySelector("#users-pick").textContent = "";
+      userChoice = document.querySelector("#options").value;
+      document.querySelector("#users-pick").textContent +=
+        "Your choice: " + userChoice;
+      return userChoice;
+    });
 }
 
 getHumanChoice();
 
-
-
-
-
 function playRound() {
-    document.getElementById('play-button').addEventListener(
-        'click',
-        function () {
-            const humanSelection = document.getElementById('options').value;
-            const computerSelection = getComputerChoice();
-            document.getElementById('computers-pick').innerHTML
-            = 'Computers choice: '+computerSelection;
-            document.getElementById('declare-winner').innerHTML
-            = ' '
-            
+  document.querySelector("#play-button").addEventListener("click", function () {
+    const humanSelection = document.querySelector("#options").value;
+    const computerSelection = getComputerChoice();
+    document.querySelector("#computers-pick").textContent =
+      "Computers choice: " + computerSelection;
+    document.querySelector("#declare-winner").textContent = " ";
 
-            let whoWon = '';
-            let humanScore = 0;
-            let computerScore = 0;
+    let whoWon = "";
+    let humanScore = 0;
+    let computerScore = 0;
 
-            if (humanSelection == 'Rock' && computerSelection == 'Scissors') {
-                document.getElementById('declare-winner').innerHTML
-                += ' Player won'
-                whoWon = 'player'
-                
-                
-            }   else if (humanSelection == computerSelection) {
-                document.getElementById('declare-winner').innerHTML
-                += 'It is a draw!'
-            }   else if (humanSelection == 'Scissors' && computerSelection == 'Paper') {
-                document.getElementById('declare-winner').innerHTML
-                += ' Player won';
-                whoWon = 'player'
-                
-            }   else if (humanSelection == 'Paper' && computerSelection == 'Rock') {
-                document.getElementById('declare-winner').innerHTML
-                += ' Player won'
-                whoWon = 'player'
-            }   else {
-                document.getElementById('declare-winner').innerHTML
-                += ' Computer won'
-                whoWon = 'computer'
-            }   
+    if (humanSelection == "Rock" && computerSelection == "Scissors") {
+      document.querySelector("#declare-winner").textContent += " Player won";
+      whoWon = "player";
+    } else if (humanSelection == computerSelection) {
+      document.querySelector("#declare-winner").textContent += "It is a draw!";
+    } else if (humanSelection == "Scissors" && computerSelection == "Paper") {
+      document.querySelector("#declare-winner").textContent += " Player won";
+      whoWon = "player";
+    } else if (humanSelection == "Paper" && computerSelection == "Rock") {
+      document.querySelector("#declare-winner").textContent += " Player won";
+      whoWon = "player";
+    } else {
+      document.querySelector("#declare-winner").textContent += " Computer won";
+      whoWon = "computer";
+    }
 
-                if (whoWon == 'player') {
-                    humanScore++;
-                    let numberBox = document.getElementById('human-score');
-                    let currentNumber = parseInt(numberBox.textContent, 0)
-                    
-                    numberBox.textContent = currentNumber + humanScore; 
-                    console.log('this is human score: ' +currentNumber);
+    if (whoWon == "player") {
+      humanScore++;
+      let numberBox = document.querySelector("#human-score");
+      let currentNumber = parseInt(numberBox.textContent, 0);
 
-                    if (currentNumber + 1 == 5) {
-                        alert('Game over, player won!')
-                        window.location.reload();
-                    }
+      numberBox.textContent = currentNumber + humanScore;
+      console.log("this is human score: " + currentNumber);
 
-                } else if (whoWon == 'computer') {
-                    computerScore++;
-                    let numBoxComputer = document.getElementById('computer-score');
-                    let currentComputerNumber = parseInt(numBoxComputer.textContent, 0)
+      if (currentNumber + 1 == 5) {
+        alert("Game over, player won!");
+        window.location.reload();
+      }
+    } else if (whoWon == "computer") {
+      computerScore++;
+      let numBoxComputer = document.querySelector("#computer-score");
+      let currentComputerNumber = parseInt(numBoxComputer.textContent, 0);
 
-                    numBoxComputer.textContent = currentComputerNumber + computerScore;
-                    
-                    if (currentComputerNumber + 1 == 5) {
-                        alert('Game over, player won!')
-                        window.location.reload();
-                    }
-                }
-        }
-    )
+      numBoxComputer.textContent = currentComputerNumber + computerScore;
+
+      if (currentComputerNumber + 1 == 5) {
+        alert("Game over, player won!");
+        window.location.reload();
+      }
+    }
+  });
 }
 
 playRound();
